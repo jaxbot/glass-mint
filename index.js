657719668f786659b079c222b674115244c838d1
Jonathan Warner <jaxbot@gmail.com>
2014-03-16 19:21:47 -0400

    Mint API code (from experimental project)

---
+++ index.js
@@ -0,0 +1,97 @@
+var https = require('https');
+var constants = require('constants');
+
+var config = require('./config.json');
+
+updateBankInfo();
+
+function updateBankInfo() {
+	pullFromMintApi("/loginUserSubmit.xevent", function(err, data) {
+		if (err) {
+			console.log("Info: request failed: " + err);
+			return;
+		}
+		var account = data.response["115485"].response[0];
+		console.log(account.currentBalance);
+		console.log(account.accountName);
+	});
+}
+
+function pullFromMintApi(path, callback) {
+	var options = {
+		hostname: 'wwws.mint.com',
+		headers: { "Accept": "application/json", "Content-type": "application/x-www-form-urlencoded; charset=UTF-8" },
+		path: path,
+		method: 'POST',
+		agent:false,
+		rejectUnauthorized:false,
+		secureOptions: constants.SSL_OP_NO_TLSv1_2,
+		strictSSL:false
+	};
+
+	var req = https.request(options, function(res) {
+		var chunk = "";
+		var loginData = {};
+
+		res.on('data', function(hunk) {
+			chunk += hunk.toString();
+		});
+		res.on('end', function() {
+			try {
+				loginData = JSON.parse(chunk);
+			} catch (e) {
+				callback(e);
+				return;
+			}
+
+			options.path = "/bundledServiceController.xevent?token=" + loginData.sUser.token;
+
+			options.headers["Cookie"] = "";
+
+			for (var i = 0; i < res.headers['set-cookie'].length;i++) {
+				options.headers["Cookie"] += res.headers['set-cookie'][i].split(';')[0].split('=')[0] + '=' + encodeURIComponent(res.headers['set-cookie'][i].split(';')[0].split('=')[1]) + "; ";
+			}
+
+			var query = https.request(options, function(res) {
+				var data = "";
+
+				res.on('data', function(chunk) {
+					data += chunk.toString();
+				});
+				res.on('end', function() {
+					try {
+						callback(null, JSON.parse(data.toString()));
+					} catch(e) {
+						callback(e);
+					}
+				});
+			});
+			query.write("input="+encodeURIComponent(JSON.stringify([
+				{
+					"args": {
+						"feature": "mintHB"
+					},
+					"service": "MintNewFeatureEnablementService",
+					"task": "isEnabled",
+					"id": "258168"
+				},
+				{
+					args: {
+						types: [
+							"BANK"
+						]
+					},
+					id: "115485",
+					service: "MintAccountService",
+					task: "getAccountsSorted"
+				}
+			])));
+			query.end();
+		});
+	});
+
+	req.write("username=" + encodeURIComponent(config.mint_username) + "&task=L&password=" + encodeURIComponent(config.mint_password) + "&browser=chrome&browserVersion=33&os=win");
+	req.end();
+
+}
+
